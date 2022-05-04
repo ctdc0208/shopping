4b2bc6fa7b1fb249abc79b3a1df27ebd55deca48
nexus0208 <christiandelacruz0208@gmail.com>
2022-05-04 11:52:25 +0800

    add footer

---
+++ src/UI/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Ui.css"
+
+class Footer extends React.Component {    
+    render() {
+        return (
+            <div className="footer-container">
+                <div className="footer">
+                    COPYRIGHT 2022
+                </div>
+            </div>
+        )
+    }
+};
+
+
+export default Footer
