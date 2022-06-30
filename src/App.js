24feced09f0d6ac57daa40d982cb3db2c62aa9a6
nexus0208 <christiandelacruz0208@gmail.com>
2022-06-30 21:53:53 +0800

    Add main.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css"
 import Header from "./UI/Header";
+import Main from './UI/Main';
 import Footer from "./UI/Footer";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
