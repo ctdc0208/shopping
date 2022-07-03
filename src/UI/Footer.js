3a332bc5c1b9f68be30a52d2d5d2c9e33512c182
nexus0208 <christiandelacruz0208@gmail.com>
2022-07-03 13:08:55 +0800

    add uniqlo footer

--- src/UI/Footer.js
+++ src/UI/Footer.js
@@ -4,11 +4,56 @@ import "./Footer.css"
 class Footer extends React.Component {    
     render() {
         return (
-            <div className="footer-container">
-                <div className="footer">
-                    COPYRIGHT 2022
+            <footer>
+                <div class="flexdiv">
+                    <div>
+                        <p>About Uniqlo</p>
+                        <p>Information</p>
+                        <p>Store Locator</p>
+                    </div>
+                    <div>
+                        <p>Help</p>
+                        <p>FAQ</p>
+                        <p>Return Policy</p>
+                        <p>Privacy Policy</p>
+                        <p>Accessibility</p>
+                    </div>
+                    <div>
+                        <p>Account</p>
+                        <p>Membership</p>
+                        <p>Profile</p>
+                        <p>Coupons</p>
+                    </div>
+                    <div>
+                        <p>E-Newsletter</p>
+                        <p>Sign up and be the first-in-the know about new arrivals, promotions, in-store events and more.</p>
+                        <p class="subscribe">SUBSCRIBE NOW</p>
+                    </div>
+                    <div>
+                        <p>UNIQLO Social Account</p>
+                        <div id="social-icons">
+                            
+                        <a href="https://www.facebook.com/uniqloin/">Facebook</a> 
+                        <a href="https://twitter.com/uniqloin">Twitter</a> 
+                        <a href="https://www.instagram.com/uniqloin/">Instagram</a> 
+                        <a href="https://www.youtube.com/c/UniqloIn">Uniqlo</a> 
+                        </div>
+                    </div>
                 </div>
-            </div>
+                <div>
+                    <div class="lang">English</div>
+                </div>
+                <div class="underline">
+                    <div>
+                        <p>
+                        COPYRIGHT © UNIQLO CO., LTD. ALL RIGHTS RESERVED.
+                    </p>
+                    </div>
+                    <div>
+                        <p><span> Terms of use  </span><span>  Privacy policy </span></p>
+                    </div>
+                </div>
+            </footer>
         )
     }
 };
