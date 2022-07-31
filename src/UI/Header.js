748c57b75710aaaced97e69d19790b11b5f21267
nexus0208 <christiandelacruz0208@gmail.com>
2022-07-31 09:58:43 +0800

    add first header

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -4,16 +4,39 @@ import "./Header.css"
 class Header extends React.Component {    
     render() {
         return (
-            <div className="header-container">
-                <button className="header">
-                    UNIQLO
-                </button>
-                <div>
-                    <button className="categories">MEN</button>
-                    <button className="categories">WOMEN</button>
-                    <button className="categories">KIDS</button>
-                </div>
+            <div>
+                <header>
+                    <div className="first-header-wrapper">
+                        <div className="first-header-contained">
+                            <div className="first-header-flex">
+                                <div className="nav-item">Help</div>
+                                <div className="nav-item">Store Locator</div>
+                                <div className="nav-item">English</div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <nav className="navbar">
+                    <div className="header-container">
+                        <div className="flex-start">
+                            <button className="logo">
+                            ユニクロ｜UNIQLO
+                            </button>
+                            <button className="categories">MEN</button>
+                            <button className="categories">WOMEN</button>
+                            <button className="categories">KIDS</button>
+                        </div>
+                        <div className="float-right">
+                            <div className="search-container">
+                                <input className="search-bar" type="text" placeholder="Search by keyword" />
+                                <button class="search-button"></button>
+                            </div>
+                            <button>Cart</button>
+                        </div>
+                    </div>
+                </nav>
             </div>
+           
         )
     }
 };
