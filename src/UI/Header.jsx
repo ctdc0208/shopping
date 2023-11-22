020858c1f7b49dbea6ec775b9f0369216d5c37ac
Christian dela Cruz <66873179+ctdc0208@users.noreply.github.com>
2023-11-22 21:48:11 +0800

    delete comment

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -58,38 +58,7 @@ class Header extends React.Component {
             <div className="sticky-header">
                 <Discount />
                 <MainHeader />
-                {/* <header>
-                    <div className="first-header-wrapper">
-                        <div className="first-header-contained">
-                            <div className="first-header-flex">
-                                <div className="nav-item">Help</div>
-                                <div className="nav-item">Store Locator</div>
-                                <div className="nav-item">English</div>
-                            </div>
-                        </div>
-                    </div>
-                </header>
-                <nav className="navbar">
-                    <div className="header-container">
-                        <div className="flex-start">
-                            <button className="logo">
-                            ユニクロ｜UNIQLO
-                            </button>
-                            <button className="categories">MEN</button>
-                            <button className="categories">WOMEN</button>
-                            <button className="categories">KIDS</button>
-                        </div>
-                        <div className="float-right">
-                            <div className="search-container">
-                                <input className="search-bar" type="text" placeholder="Search by keyword" />
-                                <button className="search-button"></button>
-                            </div>
-                            <button>Cart</button>
-                        </div>
-                    </div>
-                </nav> */}
             </div>
-           
         )
     }
 };
