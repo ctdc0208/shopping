a2a1fece7ddb6480e080b5184c59b3facb6549b1
Christian dela Cruz <66873179+ctdc0208@users.noreply.github.com>
2023-11-28 11:08:05 +0800

    add cart empty css

--- src/UI/Cart.jsx
+++ src/UI/Cart.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Icon from "@mdi/react";
 import { mdiCart } from '@mdi/js';
+import { mdiClose } from '@mdi/js';
 import "./styles/Cart.css";
+import { Link } from "react-router-dom";
 
 export const cartData = [null]
 
@@ -26,10 +28,23 @@ function Cart() {
                 <div className="opacity">
                 </div>
                 <div className='cart-container'>
-                    <li className='cart-toggle'>
-                        <a  onClick={showSidebar}>Close</a>
-                    </li>
-                    <li className='cart-content'>Contents</li>
+                    
+                        <div className='cart-header'>
+                            <div className="cart-title">Your Cart</div>
+                            <button  onClick={showSidebar}><Icon path={mdiClose} size={1}/></button>
+                        </div>
+                        <div className='cart-content'>
+                            <div className="cart-empty">Your Cart is Empty</div>
+                            <div className="cart-shop-container">
+                                <div className="cart-shop-description">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                </div>
+                                <Link to="women's clothing" aria-current="page" >Shop Women</Link>
+                                <Link to="men's clothing" aria-current="page" >Shop Men</Link>
+                                <Link to="jewelery" aria-current="page">Shop Jewelery</Link>
+                                <Link to="electronics" aria-current="page" >Shop Electronics</Link>
+                            </div>
+                        </div>
                 </div>
             </div>
             </div>
